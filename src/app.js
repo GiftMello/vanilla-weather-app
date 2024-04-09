783f0686034ea49c9b8111958c8d59951c0ddc05
GiftMello <giftmello0@gmail.com>
2024-04-10 01:31:40 +0200

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ function refreshWeather (response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElemnent = document.querySelector("#icon"); 
 
-
+  iconElemnent.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city; 
   timeElement.innerHTML = setDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
@@ -35,9 +36,6 @@ function setDate(date) {
     minutes = `0${minutes}`;
   }
 
-  if (hours < 1) {
-    hours = `0${hours}`;
-  }
 
   return `${day} ${hours}:${minutes}`;
 }
